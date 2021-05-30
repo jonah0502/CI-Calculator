9800a450fef47cdc69af96bf0c656758a8955683
jonah0502 <jonah0502@gmail.com>
2021-05-29 17:57:48 -0700

    added multiply and divide methods and tests

--- test_calulator.py
+++ test_calulator.py
@@ -10,3 +10,9 @@ class TestCalculatorApp:
 
     def test_subtract(self):
         assert 5 == calculator.subtract(10, 5)
+
+    def test_multiply(self):
+        assert 4 == calculator.multiply(2, 2)
+
+    def test_divide(self):
+        assert 5 == calculator.divide(20, 4)
